62fec09f05fbc19ef1a5aa3c9339344c3f6756a9
Julia <webdoxy@gmail.com>
2021-02-04 21:43:22 +0300

    app + tests

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 """
-app example
-xx
-
+check domain
 """
-
+import requests
 
 def is_alive_host(hostname):
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
-    pass
+    try:
+        headers = {'user-agent': 'test-app/0.0.1'}
+        host = 'http://'+hostname
+        r = requests.get(host, headers=headers, timeout=5)
+    except Exception as e:
+        return 'off'
+        # requests.exceptions.
+        # return e.__class__.__name__
+    # else:
+    #     print(r.status_code)
+
+    if  100 <= r.status_code < 400:
+        return 'on'
+    else:
+        return 'off'
+
+hostname = "ya.ru"
+ans = is_alive_host(hostname)
+
+# print(ans)
--- tests.py
+++ tests.py
@@ -2,16 +2,28 @@
 app tests example
 
 """
-
+import pytest
 from app import is_alive_host
 
+# def test_alive_host():
+#     assert is_alive_host('ya.ru') == 'on'
+#
+#
+# def test_down_host():
+#     assert is_alive_host('102.com') == 'off'
 
-def test_alive_host():
-    pass
 
-
-def test_down_host():
-    pass
-
-    
-/// etc...
+@pytest.mark.parametrize(
+    ('hostname','answer'),
+    [
+        ('ya.ru','on'),
+        ('google.com','on'),
+        ('navalny.com','on'),
+        ('102.com','off'),
+        ('noname.shop','off'),
+        ('q9lwbz65oyko.io','off'),
+        ('5dq3ti-fgb6z.edu','off'),
+    ]
+)
+def test_host(hostname, answer):
+    assert is_alive_host(hostname) == answer
